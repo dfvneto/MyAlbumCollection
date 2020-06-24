e150dc66ca79048dc780c7ed28514cf8bd652a60
Domingos Neto <dfvneto@gmail.com>
2020-06-24 05:11:32 -0300

    add the id in the serializers class

--- AlbumCollection/serializers.py
+++ AlbumCollection/serializers.py
@@ -2,14 +2,17 @@ from rest_framework import serializers
 from AlbumCollection.models import Album, Track
 
 
-
+# Track Class Serializer
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
-        fields = ['name', 'length', 'music_genre', 'album']
+        fields = ['id','name', 'length', 'music_genre', 'album']
 
+
+# Album Class Serializer
+# Using nested relationship  to represent foreign key
 class AlbumSerializer(serializers.ModelSerializer):
     tracks = TrackSerializer(many=True, read_only=True)
     class Meta:
         model = Album
-        fields = ['name', 'created', 'artist', 'tracks']
+        fields = ['id','name', 'created', 'artist', 'tracks']
