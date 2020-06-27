ce1a2360cb0064860d904b82dcd12bb6efade2aa
Domingos Neto <dfvneto@gmail.com>
2020-06-27 15:52:10 -0300

    added tests

--- AlbumCollection/models.py
+++ AlbumCollection/models.py
@@ -7,12 +7,19 @@ class Album(models.Model):
     created = models.DateField()
     name = models.TextField()
     artist = models.TextField()
+    class Meta:
+        ordering= ['id']
 
+    def __str__(self):
+        return self.name
+        
 class Track(models.Model):
     name = models.TextField()
     length = models.TimeField()
     music_genre = models.TextField()
     album = models.ForeignKey(Album,related_name="tracks", on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['id']
+    
     def __str__(self):
-        return '%s' % self.name
-
+        return self.name
--- AlbumCollection/tests.py
+++ AlbumCollection/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.urls import include, path
+from AlbumCollection.models import Album, Track
+from rest_framework import status
+from rest_framework.test import APITestCase, RequestsClient, URLPatternsTestCase, APIRequestFactory
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class AlbumTestCase(APITestCase):
+    client = APIClient()
+    def test_post_album(self):
+        # client = APIClient()
+        data ={
+            "name": "test-album",
+            "created": "2020-02-02",
+            "artist": "test-artist",
+            "tracks": []
+        }
+        response = self.client.post('/albuns/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Album.objects.count(), 1)
+        self.assertEqual(Album.objects.get().name, 'test-album')
+
+    def test_get_album(self):
+        response = self.client.get('/albuns/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TrackTestCase(APITestCase):
+
+    def test_get_track(self):
+        response = self.client.get('/tracks/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
+    # def test_post_track(self):
+    #     # url = ("http://0.0.0.0:8000/tracks/")
+    #     url = ('http://127.0.0.1:8000/tracks/')
+    #     data ={
+    #         "name": "test-track",
+    #         "length": "00:00:50",
+    #         "music_genre": "test",
+    #         "album": 1
+    #     }
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Track.objects.count(), 1) 
+    #     self.assertEqual(Track.objects.get().name, 'test-track')
